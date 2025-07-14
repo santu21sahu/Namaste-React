476ddddd1d90f233e4abed6c57117291cbcb52ab
santu21sahu <santu21sahu@gmail.com>
2025-07-14 06:52:48 +0530

    episode 01

---
+++ App.js
@@ -0,0 +1,8 @@
+
+
+const parent = React.createElement("div", {id : "parent"}, React.createElement("div", {id : "child"},
+     React.createElement("h1", {}, "I am in h1 tag")) );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
